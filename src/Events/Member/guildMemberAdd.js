15b054d01a08faee785f1ce73babdce62dca52a3
Jermayn <67791136+Jermayn1@users.noreply.github.com>
2025-07-19 15:21:46 +0200

    welcome message bearbeitet

--- src/Events/Member/guildMemberAdd.js
+++ src/Events/Member/guildMemberAdd.js
@@ -1,11 +1,11 @@
-const { GuildMember, Client } = require("discord.js");
+const { GuildMember, Client, EmbedBuilder } = require("discord.js");
 const { genWelcomeCard } = require("../../Structures/Systems/Welcome/profile-image");
 const welcomeChannel = require("../../Structures/Schemes/welcomeChannel");
 
 module.exports = {
     name: "guildMemberAdd",
     once: false,
-    /**
+    /** Welcome System
      * @param {GuildMember} member 
      * @param {Client} client 
      */
@@ -20,9 +20,35 @@ module.exports = {
 
             const card = await genWelcomeCard(client, member);
 
+            const rndMessage = await getRandomWelcomeMessage(member)
+
             channel.send({
+                content: rndMessage,
                 files: [card]
             });
         } catch(err) { console.log(err) }
     }
+}
+
+/**
+ * @param {GuildMember} member 
+ * @returns String mit der Nachricht
+ */
+async function getRandomWelcomeMessage(member) {
+    // Zufällige Willkommens Nachrichten
+    const welcomeMessages = [
+        `👋 Schön, dass du da bist, **${member}**.`,
+        `👋 Juhu, du hast es geschafft, **${member}**!`,
+        `👋 Heißen wir **${member}** herzlich willkommen!`,
+        `👋 Willkommen, **${member}**! Sag hallo!`,
+        `👋 **${member}** ist grade aufgetaucht!`,
+        `👋 **${member}** ist gerade auf den Server geschlittert.`,
+        `👋 Schön, dich zu sehen, **${member}**.`,
+        `👋 **${member}** ist der Gruppe beigetreten.`,
+        `👋 Ein wildes **${member}** erscheint!`,
+        `👋 Willkommen, **${member}**. Wir hoffen, du hast Pizza mitgebracht.`,
+        `👋 **${member}** ist gelandet.`
+    ];
+
+    return welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)] ?? `👋 Schön, dass du da bist, **${member}**.`;
 }
